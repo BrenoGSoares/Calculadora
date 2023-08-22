c7a73d56bde66edb9476175f43f42cded1569152
Breno <breno.givas@icloud.com>
2023-08-22 11:39:43 -0300

    Update: functions done

--- js/script.js
+++ js/script.js
@@ -2,3 +2,23 @@ function insert(num){
     var number = document.getElementById('value').innerHTML;
     document.getElementById('value').innerHTML = number + num
 }
+
+function clean(){
+    document.getElementById('value').innerHTML = "";
+}
+
+function del(){
+    var result = document.getElementById('value').innerHTML;
+    document.getElementById('value').innerHTML = result.substring(0, result.length -1)
+}
+
+function calc(){
+    var result = document.getElementById('value').innerHTML;
+    if(result){
+        document.getElementById('value').innerHTML = eval(result)
+    }
+    else{
+        document.getElementById('value').innerHTML = 'ERROR'
+        setTimeout(function(){clean()}, 250);
+    }
+}
